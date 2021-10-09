428a1a6cbc4eba0707fd99cc0a1828f2a3736d68
millanadhikari <millanadhikari@gmail.com>
2021-10-09 13:06:49 +1100

    adding path

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-require("dotenv").config()
+const path = require ('path');
+require("dotenv").config({path:path.resolve(__dirname, './.env')})
+
+
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
@@ -18,6 +21,7 @@ app.use(express.json())
 app.use(cors());
 
 app.use(morgan("tiny"));
+
 
 
 //set body bodyParser
