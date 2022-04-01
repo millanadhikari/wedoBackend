1c6398e1d803dc468ae65c76f5b4fab94c0e5dde
millanadhikari <millanadhikari@gmail.com>
2022-04-01 22:09:43 +1100

    New booking confirmation email template uplaoded2.0

--- src/helpers/email.helper.js
+++ src/helpers/email.helper.js
@@ -73,11 +73,14 @@ const bookingEmailProcessor = ({ bookingObj }) => {
         html: `<p>Hello</p>`,
         amp: `
         <!doctype html>
-        <html>
+        <html ⚡>
   <head>
-    <meta charset="UTF-8">
-    <script src="script.js"></script>
-    <style>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <meta name="description" content="This is the AMP Boilerplate.">
+    <link rel="preload" as="script" href="https://cdn.ampproject.org/v0.js">
+    <script async src="https://cdn.ampproject.org/v0.js"></script>
+    <style amp-custom>
       .container {
           font-size: 16px;
           font-family: Arial;
@@ -230,6 +233,8 @@ const bookingEmailProcessor = ({ bookingObj }) => {
         margin-left:6px
       }
     </style>
+    <style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
+    <link rel="canonical" href=".">
     <link rel="stylesheet" type="text/css" href="styles.css">
   </head>
   <body>
